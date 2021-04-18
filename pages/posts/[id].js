4ce05f2b1e5e677ab0b9a54cb3a3eb1a40e20814
ykyp <yannakyp@hotmail.com>
2021-04-18 22:08:49 +0300

    fix dates

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -13,7 +13,7 @@ export default function Post({ postData }) {
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
+          <Date dateString={postData.startDate} /> - <Date dateString={postData.endDate} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
