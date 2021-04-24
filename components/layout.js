7a31df78fe2d47484d860607eb87c61b9b242013
ykyp <yannakyp@hotmail.com>
2021-04-24 23:20:39 +0300

    some metadata changes

--- components/layout.js
+++ components/layout.js
@@ -14,13 +14,11 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Find all theatre events in Cyprus"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://i.pinimg.com/originals/fb/86/15/fb8615290f9d95e38e701377b3d3e78d.png`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
