ade7bb023f782a8ab159ed058b7af5de3b2c4182
ykyp <yannakyp@hotmail.com>
2021-04-25 17:38:37 +0300

    change logos

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/theatralis.jpeg"
+              src="/images/thoc.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
