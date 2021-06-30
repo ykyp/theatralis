8aaa9a3dd640180dcd47ca09f8fac4696b2400e2
Ioanna Kyprianou <ioanna.kyprianou@sky.uk>
2021-06-30 22:43:34 +0300

    added tabs

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -3,6 +3,7 @@ import { getAllEventIds, getEventData } from '../../lib/events'
 import Head from 'next/head'
 import FormattedDate from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function Event({ eventData: eventData }) {
   return (
@@ -10,14 +11,30 @@ export default function Event({ eventData: eventData }) {
       <Head>
         <title>{eventData.title}</title>
       </Head>
+
+
+
       <div className="w-full flex justify-around">
+
         <article className="prose prose-purple">
           <h1 className={utilStyles.headingXl}>{eventData.title}</h1>
           <div className={utilStyles.lightText}>
             {/*<FormattedDate dateString={eventData.startDate} /> - <FormattedDate dateString={eventData.endDate} />*/}
           </div>
           <h3>For {eventData.audience}</h3>
-          <div dangerouslySetInnerHTML={{ __html: eventData.contentHtml }} />
+
+          <div className="hide-li">
+            <TabView>
+              <TabPanel header="About">
+                <div dangerouslySetInnerHTML={{ __html: eventData.contentHtml }} />
+              </TabPanel>
+              <TabPanel header="Map">
+                <p>Map will go here...</p>
+              </TabPanel>
+            </TabView>
+          </div>
+
+
         </article>
       </div>
     </Layout>
