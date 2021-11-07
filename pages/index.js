9c4292b28edb2fb8a6d354a79c66b44ecff12356
Ioanna Kyprianou <ioanna.kyprianou@sky.uk>
2021-11-07 23:19:13 +0200

    better ga monitoring

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,7 @@ export default function Home({ allEventsData }) {
       ga.event({
          action: "search",
          params : {
-            search_term: selectedCity.code
+            search_term: selectedCity.name + "-" + selectedPeriod.name + "-"+ selectedCategory.name
          }
       })
    };
