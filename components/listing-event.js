e21f29e6d73cbd9954aa9fd531001be74e56ca77
Ioanna Kyprianou <ioanna.kyprianou@sky.uk>
2021-12-11 14:43:29 +0200

    tidy up

--- components/listing-event.js
+++ components/listing-event.js
@@ -1,30 +1,9 @@
-import styled from 'styled-components'
 import useTranslation from 'next-translate/useTranslation';
 import React from 'react';
 import {formatDate} from "./date";
 import { useRouter } from 'next/router';
 import Link from 'next/link'
 
-const FiltersContainer = styled.section`
-      display: flex;
-      justifyContent: normal;      
-   `;
-
-const WithMargin = styled.div`
-      margin-left: 25px;      
-   `;
-
-/*
-* {/*<div className={utilStyles.listItem} key={id}>
-         <Link href={`/events/${id}`}>
-            <a>{title}</a>
-         </Link>
-         <br />
-         <small className={utilStyles.lightText}>
-            {city}, <FormattedDate dateString={startDate} /> - <FormattedDate dateString={endDate} />
-         </small>
-         { event_image && <img width={35} height={35} src={event_image}/> }
-      </div>*/
 export const ListingEvent = (props) => {
    const { t, lang } = useTranslation('common');
    const { id, startDate, endDate, title, city, event_image, category } = props.event;
@@ -64,18 +43,24 @@ export const ListingEvent = (props) => {
                <div className="mt-2 text-gray-500">
                   <div className="mt-1 xs:text-sm text-sm sm:text-sm  xs:mobile-card-text flex items-center">
                      <i className="pi pi-calendar th-icon"></i>
-                     <div className="th-icon-text">{formatDate(startDate)} - {formatDate(endDate)}</div>
+                     <div className="th-icon-text">
+                        {formatDate(startDate)} - {formatDate(endDate)}
+                     </div>
                   </div>
 
                   {category &&
                   <div className="mt-1 xs:text-sm text-sm sm:text-sm xs:mobile-card-text flex items-center">
                      <i className="pi pi-tag th-icon"></i>
-                     <div className="xs:line-clamp-1 xs:truncate xs:w-48">{translatedKeys(category)}</div>
+                     <div className="xs:line-clamp-1 xs:truncate xs:w-48">
+                        {translatedKeys(category)}
+                     </div>
                   </div>
                   }
                   <div className="mt-1 xs:text-sm text-sm sm:text-sm xs:mobile-card-text flex items-center">
                      <i className="pi pi-map-marker th-icon"></i>
-                     <div className="xs:line-clamp-1 xs:truncate xs:w-48">{translatedKeys(city)}</div>
+                     <div className="xs:line-clamp-1 xs:truncate xs:w-48">
+                        {translatedKeys(city)}
+                     </div>
                   </div></div>
             </div>
             {/*<p className="read-more">
@@ -84,52 +69,6 @@ export const ListingEvent = (props) => {
          </div>
 
       </div>
-
-      {/*<Card style={{ width: '100%',
-         marginBottom: '1em',
-         borderBottom: '2px solid #780811'
-      }}>
-
-         <div className="grid grid-cols-3">
-         <div className="flex flex-nowrap">
-            <div style={{cursor: 'pointer'}} onClick={() => router.push(`/events/${id}`)}>
-               { event_image ?
-                  <img width={200}
-                       className="object-cover"
-                       height={150}
-                       style={{ margin: 'auto'}}
-                       src={event_image}/> :
-                  <img width={200}
-                       height={150}
-                       className=""
-                       style={{ margin: 'auto'}}
-                       src="/images/theatralis-sm-white.png"/>}
-            </div>
-
-            <div className="p-col-8 pt-3 pl-8 pr-8 pb-8" style={{lineHeight: '1.5'}}>
-               <h3  className="formatted-h3">
-                  <Link  href={`/events/${id}`}>
-                     <a className='brand-red uppercase'>{title}</a>
-                  </Link>
-               </h3>
-               <div className="p-card-subtitle md:text-sm  sm:mobile-card-text flex items-center">
-                  <i className="pi pi-calendar th-icon"></i>
-                  <div className="th-icon-text">{formatDate(startDate)} - {formatDate(endDate)}</div>
-               </div>
-               {category &&
-                  <div className="p-card-subtitle md:text-sm  sm:mobile-card-text flex items-center">
-                     <i className="pi pi-tag th-icon"></i>
-                     <div className="th-icon-text">{translatedKeys(category)}</div>
-                  </div>
-               }
-               <div className="p-card-subtitle md:text-sm  sm:mobile-card-text flex items-center">
-                  <i className="pi pi-map-marker th-icon"></i>
-                  <div className="th-icon-text">{translatedKeys(city)}</div>
-               </div>
-            </div>
-
-         </div>
-      </Card>*/}
       </>
    );
 };
