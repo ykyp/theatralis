bad74e0ac3d023b14346dc130550a1b5abb5b463
Ioanna Kyprianou <ioanna.kyprianou@sky.uk>
2021-12-12 11:51:03 +0200

    made with love

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -5,6 +5,7 @@ const Footer = () => (
    <footer className="max-w-screen-md px-3 py-4 mx-auto text-gray-500 text-sm grid grid-cols-1 gap-y-3 sm:grid-cols-2">
       <div className="text-center sm:text-left">
          {`© Copyright ${new Date().getFullYear()} Theatralis`}
+         <span> - Made with <i className="pi th-icon pi-heart"></i></span>
       </div>
 
       {/*<FooterIconList />*/}
