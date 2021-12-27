fbe0f88c419b9e897aa827050d6faf0417fcc217
ykyp <yannakyp@hotmail.com>
2021-12-27 18:33:45 +0200

    preload fix

--- components/layout.js
+++ components/layout.js
@@ -45,16 +45,16 @@ export default function Layout({ children, home, pageTitle, description, current
          <meta property="og:description" content={fbSiteName || description || defaultDesc} key="ogdesc" />
          <meta property="og:type" content="website" />
 
-         <link rel="preload" href="/images/about1.png" as="image"/>
-         <link rel="preload" href="/images/about2.png" as="image"/>
-         <link rel="preload" href="/images/about3.png" as="image"/>
-         <link rel="preload" href="/images/background-cropped.jpg" as="image"/>
-         <link rel="preload" href="/images/th7.png" as="image"/>
          <link rel="preload" href="/images/xmas-en-2.png" as="image"/>
          <link rel="preload" href="/images/xmas-gr-2.png" as="image"/>
          <link rel="preload" href="/images/xmas-background.png" as="image"/>
+         <link rel="preload" href="/images/about1.png" as="image"/>
+         <link rel="preload" href="/images/about2.png" as="image"/>
+         <link rel="preload" href="/images/about3.png" as="image"/>
+         {/*<link rel="preload" href="/images/background-cropped.jpg" as="image"/>
          <link rel="preload" href="/images/theatralis_greek_white.png" as="image"/>
          <link rel="preload" href="/images/theatralis_english_white.png" as="image"/>
+          <link rel="preload" href="/images/th7.png" as="image"/>*/}
 
       </Head>
 
