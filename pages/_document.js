8e774be0d7183501941be547d9dd5b8aad2b669e
ykyp <yannakyp@hotmail.com>
2022-02-05 21:18:46 +0200

    wip

--- pages/_document.js
+++ pages/_document.js
@@ -5,38 +5,6 @@ export default class MyDocument extends Document {
       return (
          <Html>
          <Head>
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <script
-               async
-               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
-            <script
-               dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0TQL1NZTJ0', {
-              page_path: window.location.pathname,
-            });
-          `,
-               }}
-            />
-
-            <script async
-                    src="https://www.googletagmanager.com/gtag/js?id=UA-196728460-1"/>
-            <script dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments);}
-               gtag('js', new Date());
-
-               gtag('config', 'UA-196728460-1');
-          `,
-         }}
-            />
-
-
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital@0;1&display=swap"
