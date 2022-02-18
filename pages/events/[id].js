4ff4bde69b34a741c166e07c1992bb11781c3498
ykyp <yannakyp@hotmail.com>
2022-02-18 21:06:08 +0200

    include title

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -257,15 +257,25 @@ export default function Event({ eventData: eventData }) {
                 <div className="article-container">
                   {/*<hr/>
                   <h4> Comments </h4>*/}
+
                   <div className="event-details flex items-center mb-5 ">
                     <i className="pi pi-arrow-circle-right mr-2 th-24"></i>
                     ️ <div className="th-icon-text font-bold"> {t("criticPhilenews")}</div>
                   </div>
 
                   <a href={eventData.critic_link} target="_blank">
-                    <img src="/images/phile_logo.png" className="text-center m-auto" alt="Phileleftheros logo"
+                    <img src="/images/phile_logo.png" className="text-center m-auto pb-5" alt="Phileleftheros logo"
                        style={{ width: '150px', maxHeight: '500px', display: 'block', objectFit: 'contain' }}/>
                   </a>
+
+                  <div className=" justify-center text-center">
+                    <strong className="justify-center text-center">
+
+                        { eventData.critic_title }
+                    </strong>
+                  </div>
+
+
 
                   <div className="event-body md:text-justify lg:text-justify xl:text-justify xxl:text-justify xs:text-sm sm:text-sm "
                        dangerouslySetInnerHTML={{ __html: eventData.critic_p }} />
