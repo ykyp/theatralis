0d85eac194a7118729b08dfb5867f6a46d510095
ykyp <yannakyp@hotmail.com>
2022-04-26 16:31:28 +0300

    wip

--- pages/venues/index.js
+++ pages/venues/index.js
@@ -6,12 +6,16 @@ import {BackToHome} from "../../components/navigation/backToHome";
 import {getSortedVenuesData} from "../../lib/theatres";
 import Link from 'next/link'
 import {GMap} from "primereact/gmap";
+import _ from 'lodash';
 
 export default function Venues({allVenuesData}) {
    const {t} = useTranslation('common');
    const [overlays, setOverlays] = useState(null);
    const CYPRUS_LAT = 35.1264;
    const CYPRUS_LONG = 33.4299;
+
+   const byCity = _.groupBy(allVenuesData, 'city' );
+   console.log(byCity);
 
    const contentString = (t) => {
       return '<div id="content">' +
@@ -36,7 +40,7 @@ export default function Venues({allVenuesData}) {
          const marker = new google.maps.Marker({
             position: {lat: lat, lng: long},
             title: t.name,
-            icon: `${process.env.BASE_URL}/images/theatralis_pin.png`
+            icon: `http://www.theatralis.com.cy/images/theatralis_pin.png`
          });
 
          const infowindow = new google.maps.InfoWindow({
@@ -87,15 +91,20 @@ export default function Venues({allVenuesData}) {
                onMapReady={onMapReady} />
 
          <div className="w-full flex justify-around">
-            <article className="policies prose p-3 xs:m-5  md:text-justify lg:text-justify xl:text-justify xxl:text-justify prose max-w-sm lg:max-w-3xl md:max-w-3xl  sm:max-w-2xl xs:max-w-l ">
-               <h2 >Venues</h2>
-                  {allVenuesData.map((t, i) =>
-                     <p>
-                        <Link  href={`/venues/${t.id}`}>
-                           <a className='brand-red uppercase xs:text-sm '>{t.name}</a>
-                        </Link>
-                     </p>
-                     // <TheatreInfo theatreData={t} key={i}/>
+            <article className="policies  p-3 xs:m-5  md:text-justify lg:text-justify xl:text-justify xxl:text-justify  max-w-sm lg:max-w-3xl md:max-w-3xl  sm:max-w-2xl xs:max-w-l ">
+               <h4>Venues By City</h4>
+                  {Object.keys(byCity).map((city, i) =>
+                     <>
+                       <h4>{city}</h4>
+                        {byCity[city].map((t, i) =>
+                              <p>
+                                 <Link  href={`/venues/${t.id}`}>
+                                    <a className='brand-red uppercase xs:text-sm '>{t.name}</a>
+                                 </Link>
+                              </p>
+                           // <TheatreInfo theatreData={t} key={i}/>
+                        )}
+                        </>
                   )}
 
                <BackToHome/>
