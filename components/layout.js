7732bfed0b74210bd3986c0ba307aafbe474cdb8
ykyp <yannakyp@hotmail.com>
2022-04-28 20:09:49 +0300

    wip

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,8 @@ import useTranslation from 'next-translate/useTranslation'
 import { navLinks } from "./nav-data";
 import { useRouter } from 'next/router';
 import CookieConsent from "react-cookie-consent";
+import { useState } from 'react';
+import {ISSERVER} from "./session-storage-state";
 
 
 export default function Layout({ children, home, pageTitle, description, currentURL, previewImage, siteName, fbTitle, fbSiteName, onSearchChange, searchBy }) {
@@ -14,6 +16,11 @@ export default function Layout({ children, home, pageTitle, description, current
    const siteTitle = t('defaultSiteTitle');
    const defaultDesc = t('defaultSiteDesc');
    const defaultImage = '/images/defaultHomeImg.png';
+   const [agendaOpened, setAgendaOpened] = useState(false);
+
+   const handleAgendaClick = () => {
+      setAgendaOpened(!agendaOpened);
+   };
 
    const router = useRouter();
   return (
@@ -56,6 +63,25 @@ export default function Layout({ children, home, pageTitle, description, current
       </Head>
 
        <Navbar>
+          <li className="ml-2 mt-1" key="agenda">
+              <i className="pi pi-calendar" onClick={handleAgendaClick}></i>
+             <div className="flex justify-center ">
+                   {agendaOpened && <div className="absolute xs:right-8 right-22 mt-2 bg-white rounded-md shadow-lg overflow-hidden z-20" style={{width:'20rem'}}>
+                <div className="py-2">
+                   <a href="#" className="flex items-center px-4 py-3 border-b hover:bg-gray-100 -mx-2">
+                      <img className="h-8 w-8 rounded-full object-cover mx-1" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="avatar"/>
+                         <p className="text-gray-600 text-sm mx-2">
+                            <span className="font-bold" href="#">Sara Salah</span> replied on the <span className="font-bold text-blue-500" href="#">Upload Image</span> artical . 2m
+                         </p>
+                   </a>
+                </div>
+                <a href="#" className="block bg-gray-800 text-white text-center font-bold py-2">See all notifications</a>
+             </div> }
+             </div>
+          </li>
+          <li className="ml-2 brand-red"  key="divider-1">
+             |
+          </li>
           {navLinks.map((link, index) => {
              return (
                 <>
