e7a7cddf0d353ca7180d0dc234a20f12cbbbeaba
ykyp <yannakyp@hotmail.com>
2022-09-18 22:21:47 +0300

    wip

--- pages/_document.js
+++ pages/_document.js
@@ -5,36 +5,36 @@ export default class MyDocument extends Document {
       return (
          <Html>
          <Head>
-            {/* Global Site Tag (gtag.js) - Google Analytics */}
-            <script
-               async
-               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
-            <script
-               dangerouslySetInnerHTML={{
-                  __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-0TQL1NZTJ0', {
-              page_path: window.location.pathname,
-            });
-          `,
-               }}
-            />
+            {/*/!* Global Site Tag (gtag.js) - Google Analytics *!/*/}
+            {/*<script*/}
+               {/*async*/}
+               {/*src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}*/}
+            {/*/>*/}
+            {/*<script*/}
+               {/*dangerouslySetInnerHTML={{*/}
+                  {/*__html: `*/}
+            {/*window.dataLayer = window.dataLayer || [];*/}
+            {/*function gtag(){dataLayer.push(arguments);}*/}
+            {/*gtag('js', new Date());*/}
+            {/*gtag('config', 'G-0TQL1NZTJ0', {*/}
+              {/*page_path: window.location.pathname,*/}
+            {/*});*/}
+          {/*`,*/}
+               {/*}}*/}
+            {/*/>*/}
 
-            <script async
-                    src="https://www.googletagmanager.com/gtag/js?id=UA-196728460-1"/>
-            <script dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments);}
-               gtag('js', new Date());
+            {/*<script async*/}
+                    {/*src="https://www.googletagmanager.com/gtag/js?id=UA-196728460-1"/>*/}
+            {/*<script dangerouslySetInnerHTML={{*/}
+            {/*__html: `*/}
+            {/*window.dataLayer = window.dataLayer || [];*/}
+               {/*function gtag(){dataLayer.push(arguments);}*/}
+               {/*gtag('js', new Date());*/}
 
-               gtag('config', 'UA-196728460-1');
-          `,
-         }}
-            />
+               {/*gtag('config', 'UA-196728460-1');*/}
+          {/*`,*/}
+         {/*}}*/}
+            {/*/>*/}
 
             <script async defer
             src="https://maps.google.com/maps/api/js?key=AIzaSyDJFw0VYvFk9VqhQjDfNqmITaKYXga3SyI"/>
